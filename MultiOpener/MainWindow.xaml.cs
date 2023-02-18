1823e3e8476c5fd8225d7477e9fca3f8877efe11
FaNim21 <filipekbwo@wp.pl>
2023-02-18 20:55:04 +0100

    Dodaj pliki projektów.

---
+++ MultiOpener/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiOpener
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void UpdateButtonClick(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("POGCHAMPIK");
+        }
+
+        #region Header
+        private void HeaderMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+        private void MinimizeButtonsClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+    }
+}
