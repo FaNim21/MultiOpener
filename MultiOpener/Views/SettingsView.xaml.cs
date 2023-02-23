00ad806e74b88a9f60feb9fb806d06d97bc54c94
FaNim21 <filipekbwo@wp.pl>
2023-02-23 18:15:00 +0100

    List for open items prototyping

--- MultiOpener/Views/SettingsView.xaml.cs
+++ MultiOpener/Views/SettingsView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using MultiOpener.ListView;
 
 namespace MultiOpener.Views
@@ -10,14 +11,6 @@ namespace MultiOpener.Views
         public SettingsView()
         {
             InitializeComponent();
-        }
-
-        private void TextBlockMouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed && sender is FrameworkElement frameworkElement)
-            {
-                DragDrop.DoDragDrop(frameworkElement, new DataObject(DataFormats.Serializable, frameworkElement.DataContext), DragDropEffects.Move);
-            }
         }
 
         private void AddNewOpenItem(object sender, RoutedEventArgs e)
@@ -38,6 +31,46 @@ namespace MultiOpener.Views
         private void LoadButtonClick(object sender, RoutedEventArgs e)
         {
             //TODO: Wczytac json do listy
+        }
+
+
+        private void TextBlockMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && sender is FrameworkElement frameworkElement)
+            {
+                DragDrop.DoDragDrop(frameworkElement, new DataObject(DataFormats.Serializable, frameworkElement.DataContext), DragDropEffects.Move);
+            }
+        }
+
+        private void TextBlockDragOver(object sender, DragEventArgs e)
+        {
+            if (sender is FrameworkElement element)
+            {
+                /*TargetTodoItem = element.DataContext;
+                InsertedTodoItem = e.Data.GetData(DataFormats.Serializable);
+
+                TodoItemInsertedCommand?.Execute(null);*/
+            }
+        }
+
+        private void ItemListDragOver(object sender, DragEventArgs e)
+        {
+            object todoItem = e.Data.GetData(DataFormats.Serializable);
+            //itemList.Items.Add(new OpenListItem(todoItem.ToString()));
+        }
+
+        private void ItemListDragLeave(object sender, DragEventArgs e)
+        {
+            /*HitTestResult result = VisualTreeHelper.HitTest(lvItems, e.GetPosition(lvItems));
+
+            if (result == null)
+            {
+                if (TodoItemRemovedCommand?.CanExecute(null) ?? false)
+                {
+                    RemovedTodoItem = e.Data.GetData(DataFormats.Serializable);
+                    TodoItemRemovedCommand?.Execute(null);
+                }
+            }*/
         }
     }
 }
