8a522b77f479c21efd7ab6aee4bf3683778ab109
FaNim21 <filipekbwo@wp.pl>
2023-04-17 23:25:36 +0200

    Updated checking for update

--- MultiOpener/Utils/UpdateChecker.cs
+++ MultiOpener/Utils/UpdateChecker.cs
@@ -1,67 +1,55 @@
-﻿using NuGet.Versioning;
-using System;
+﻿using System;
 using System.Diagnostics;
-using System.Net.Http;
-using System.Text.Json;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using NuGet.Versioning;
+using Octokit;
 
 namespace MultiOpener.Utils
 {
-    public class GitHubRelease
-    {
-        public string tag_name { get; set; }
-        public string name { get; set; }
-        public string body { get; set; }
-        public string html_url { get; set; }
-        // Add other properties as needed
-    }
-
     public class UpdateChecker
     {
         private const string OWNER = "FaNim21";
         private const string REPO = "MultiOpener";
 
-        private const string GitHubApiUrl = $"https://api.github.com/repos/{OWNER}/{REPO}/releases";
-
         public async Task CheckForUpdates()
         {
-            using (var httpClient = new HttpClient())
+            try
             {
-                httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("MultiOpener"); // Add your app name as User Agent
-                var response = await httpClient.GetAsync(GitHubApiUrl);
-                if (response.IsSuccessStatusCode)
+                var gitHubClient = new GitHubClient(new ProductHeaderValue("YourAppName"));
+                var releases = await gitHubClient.Repository.Release.GetAll(OWNER, REPO);
+
+                var latestRelease = releases.OrderByDescending(r => r.PublishedAt).FirstOrDefault();
+
+                if (latestRelease != null && !IsUpToDate(latestRelease.TagName))
                 {
-                    var json = await response.Content.ReadAsStringAsync();
-                    GitHubRelease release = null;
-                    try
+                    ShowNotification("Update Available", $"Version {latestRelease.TagName} is now available. Click here to download.", () =>
                     {
-                        //TODO: 2 nie dziala deserializowanie json przez fakt ze ten string jest bledny?
-                        release = JsonSerializer.Deserialize<GitHubRelease>(json);
-                    }
-                    catch(Exception e)
-                    {
-                        MessageBox.Show(e.ToString());
-                    }
-
-                    Trace.WriteLine(release.tag_name);
-
-                    if (IsNewerVersionAvailable(release.tag_name, Consts.Version))
-                        ShowUpdateNotification(release.name, release.body, release.html_url);
+                        Process.Start(new ProcessStartInfo(latestRelease.HtmlUrl)
+                        {
+                            UseShellExecute = true
+                        });
+                    });
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to check for updates: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
-        private bool IsNewerVersionAvailable(string latestVersion, string currentVersion)
+        private bool IsUpToDate(string latestTag)
         {
-            var latest = new NuGetVersion(latestVersion);
-            var current = new NuGetVersion(currentVersion);
+            NuGetVersion latest = new(latestTag);
+            NuGetVersion current = new(Consts.Version[1..]);
+            
             return latest > current;
         }
 
-        private void ShowUpdateNotification(string releaseName, string releaseNotes, string releaseUrl)
+        private void ShowNotification(string title, string message, Action onClickAction)
         {
-            Trace.WriteLine(releaseName + "\n" + releaseUrl);
+            onClickAction.Invoke();
         }
     }
 }
