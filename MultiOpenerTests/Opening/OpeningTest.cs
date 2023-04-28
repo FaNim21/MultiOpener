a818ec3168389a30f78d7ace6b38fd746a2777c9
Filip Gorczyca <filipekbwo@wp.pl>
2023-04-28 12:58:14 +0200

    fixes and changes with unit testing

---
+++ MultiOpenerTests/Opening/OpeningTest.cs
@@ -0,0 +1,38 @@
+﻿using MultiOpener;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+
+namespace MultiOpenerTests.Opening;
+public class OpeningTest
+{
+    private Application _app;
+    private Window _window;
+
+    [SetUp]
+    public void SetUp()
+    {
+        // Start the application and get a reference to the window
+        _app = Application.Launch("MultiOpener.exe");
+        _window = _app.GetWindow("MultiOpener");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Close the window and the application
+        _window.Close();
+        _app.Close();
+    }
+
+    [Test]
+    public void WindowIsOpenedCorrect()
+    {
+        //Assert
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
