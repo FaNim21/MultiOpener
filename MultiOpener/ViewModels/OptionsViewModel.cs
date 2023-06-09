47efaaaa6c1281f52f637c944cd9235bf3bd6ef8
FaNim21 <filipekbwo@wp.pl>
2023-06-10 00:15:57 +0200

    preparations for options

--- MultiOpener/ViewModels/OptionsViewModel.cs
+++ MultiOpener/ViewModels/OptionsViewModel.cs
@@ -2,6 +2,17 @@
 {
     public class OptionsViewModel : BaseViewModel
     {
+        private int _timeLookingForInstancesData;
+        public int TimeLookingForInstancesData
+        {
+            get { return _timeLookingForInstancesData; }
+            set
+            {
+                _timeLookingForInstancesData = value;
+                OnPropertyChanged(nameof(TimeLookingForInstancesData));
+            }
+        }
+
         public OptionsViewModel()
         {
 
